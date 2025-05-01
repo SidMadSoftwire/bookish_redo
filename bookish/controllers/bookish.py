a2475d60dec19b8e9319fd5001ff6ab8282a561b
RTookey <rachel.tookey@softwire.com>
2025-05-01 13:50:09 +0100

    Feat: endpoints for all books, all users, search user by id and search book by id

--- bookish/controllers/bookish.py
+++ bookish/controllers/bookish.py
@@ -1,5 +1,6 @@
 from flask import request
 from bookish.models.book import Book
+from bookish.models.user import User
 from bookish.app import db
 from flask import Blueprint
 
@@ -13,7 +14,7 @@ def health_check():
 
 
 @bookish.route('/book', methods=['POST', 'GET'])
-def handle_example():
+def get_all_books():
     if request.method == 'POST':
         if request.is_json:
             data = request.get_json()
@@ -25,15 +26,46 @@ def handle_example():
             return {"error": "The request payload is not in JSON format"}
 
     elif request.method == 'GET':
-        examples = Book.query.all()
+        books = Book.query.all()
         results = [
                 {
-                    'id': example.id,
-                    'title': example.Title,
-                    'author': example.Author,
-                    'isbn' : example.ISBN,
-                    'quantity' : example.Quantity
-                } for example in examples]
-        return {"examples": results}
+                    'id': book.id,
+                    'title': book.Title,
+                    'author': book.Author,
+                    'isbn' : book.ISBN,
+                    'quantity' : book.Quantity
+                } for book in books]
+        return {"books": results}
     else:
         return {"error" : "request method not supported"}
+
+
+@bookish.route('/book/<id>', methods=['GET'])
+def get_book_by_id(id):
+    book = Book.query.get(id)
+    if book is None:
+        return {"error": "Book not found"}
+    else:
+        return {'id' : book.id, 'title': book.Title, 'author': book.Author, 'isbn' : book.ISBN, 'quantity' : book.Quantity}
+
+
+@bookish.route('/user', methods=['POST', 'GET'])
+def get_all_users():
+    users = User.query.all()
+    results = [
+                {
+                    'id': user.id,
+                    'name': user.Name,
+                    'limit': user.Limit,
+                    'books': [{ 'title' : book.Title } for book in user.books]
+                } for user in users]
+    return {"users": results}
+
+
+@bookish.route('/user/<int:id>', methods=['GET'])
+def get_user_by_id(id):
+    user = User.query.get(id)
+    if user is None:
+        return {"error": "User does not exist"}
+    else:
+        return {"id": user.id, "name": user.Name, "limit": user.Limit, "books": [{ 'title' : book.Title } for book in user.books]}
